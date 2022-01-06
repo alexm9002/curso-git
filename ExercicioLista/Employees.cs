70135e6ef4015e1956b58b3510303d0975f74014
Alex Marcelo <alexm9002@gmail.com>
2022-01-06 19:00:55 -0300

    incluir lista de arquivos que serão ignorados no commit

--- ExercicioLista/Employees.cs
+++ ExercicioLista/Employees.cs
@@ -2,29 +2,35 @@
 using System.Collections.Generic;
 using System.Globalization;
 
-namespace ExercicioLista {
-    internal class Employees {
+namespace ExercicioLista
+{
+    internal class Employees
+    {
 
         public int Id { get; set; }
         public string Name { get; set; }
-        public double Salario { get; private set;}
-        
-        public Employees() {
+        public double Salario { get; private set; }
+
+        public Employees()
+        {
         }
-        public Employees(int id, string name, double salario) {
+        public Employees(int id, string name, double salario)
+        {
 
             Id = id;
             Name = name;
             Salario = salario;
         }
 
-        public void AumentarSalario(double percentual) {
-            
+        public void AumentarSalario(double percentual)
+        {
+
             Salario += Salario * percentual / 100.0;
 
         }
 
-        public override string ToString() {
+        public override string ToString()
+        {
 
             return Id
                 + ", "
