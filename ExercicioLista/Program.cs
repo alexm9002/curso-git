4d7a11c0ec00d21aa530411c1d17605d80fde8c1
Alex Marcelo <alexm9002@gmail.com>
2022-01-06 17:43:35 -0300

    Etapa 01 Lista

---
+++ ExercicioLista/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace ExercicioList {
+    class Program {
+        static void Main(string[] args) {
+
+            
+
+            Console.Write("How many employees will bee registered? ");
+            int nun = int.Parse(Console.ReadLine());
+
+            // List<string> list = new List<string>();
+
+            for (int i = 1; i < nun; i++) {
+                Console.WriteLine("Employee #" + i + " :");
+                Console.Write("ID: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Salário: ");
+                double salario = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                
+                
+             //   List.add(new Func(id, name, salario));
+            }
+
+            Console.WriteLine("Updated list of Employees:");
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
